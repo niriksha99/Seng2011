82aa9781c2e96d16e91f082753a85bc5ba1d37a9
nhat286 <thiennhatnguyenminh@gmail.com>
2018-08-28 21:07:56 +1000

    render html templates

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -8,25 +10,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) 
 {
-	res.render('home.ejs');
+	res.render('home.html');
 });
 
 app.post('/', function(req, res) 
 {
-	res.render('welcome.ejs');
+	res.render('welcome.html');
 });
 
 
 app.get('/dashboard', function(req, res) 
 {
-	res.render('dashboard.ejs');
+	res.render('dashboard.html');
 });
 
 app.post('/welcome', function(req, res) 
 {
 	var name = req.body.name;
 	console.log(name);
-	res.render('welcome.ejs', {name:name});
+	res.render('welcome.html', {name:name});
 });
 
 
