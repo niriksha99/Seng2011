31e7b821640e7a14c2c545cb1761bf5c4beb324f
BlaiseParadeza <z5160298@ad.unsw.edu.au>
2018-09-07 14:37:10 +1000

    database initialised

--- model.js
+++ model.js
@@ -56,19 +56,30 @@ function create_tables() {
 		password: "password",
 		database: "PartyWhip"
 	});
-	basic_query("CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");	
+	basic_query("CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");	
+	basic_query("CREATE TABLE Requests (id INT AUTO_INCREMENT PRIMARY KEY, userID INT, date DATE, time TIME, deadline DATE, suburb VARCHAR(255), type VARCHAR(255), noPeople INT, qualityLevel VARCHAR(255), budget FLOAT(10,2), additional_info VARCHAR(255), completed BIT, FOREIGN KEY (userID) REFERENCES Users(id)) ", "created Requests table");
+	// not sure if foreign key is set up properly
+
+	basic_query("CREATE TABLE Businesses (id INT AUTO_INCREMENT PRIMARY KEY, userID INT, opening_hours VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255), description VARCHAR(255), FOREIGN KEY (userID) REFERENCES Users(id))", "created Businesses table");
+	// link userID to Users(id)
+
+	basic_query("CREATE TABLE Bids(requestID INT, businessID INT, price FLOAT(10,2), status BIT, FOREIGN KEY (requestID) REFERENCES Requests(id), FOREIGN KEY (businessID) REFERENCES Businesses(id), CONSTRAINT pk PRIMARY KEY(requestID, businessID)) ", "created Bids table");
+	// link businessID and reqID to Businesses(id) and Requests(id)
 }
 
-
 function basic_query(sql, message) {	
-	con.connect(function(err) {
+	con.query(sql, function (err, result) {
 		if (err) throw err;
-		console.log("Connected!");
-		con.query(sql, function (err, result) {
-			if (err) throw err;
-			console.log(message);
-		});
+		console.log(message);
 	});
+//	con.connect(function(err) {
+//		if (err) throw err;
+//		console.log("Connected!");
+//		con.query(sql, function (err, result) {
+//			if (err) throw err;
+//			console.log(message);
+//		});
+//	});
 }
 
 // ***** end of db stuff *****
