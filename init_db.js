b6e020c91a2d3c1c46721694c795406962ab59f6
BlaiseParadeza <z5160298@ad.unsw.edu.au>
2018-09-07 12:16:20 +1000

    initialising db works (in other file)

--- init_db.js
+++ init_db.js
@@ -1,14 +1,17 @@
 var mysql = require('mysql');
+var con;
 
-initialise_database();
+initialise_database(create_tables);
 
-function initialise_database() {
+function initialise_database(callback) {
 	var initial_con = mysql.createConnection({
 		host: "localhost",
 		user: "root",
 		password: "password"
 	});
 
+//	basic_query(initial_con, "DROP DATABASE IF EXISTS PartyWhip", "Old PartyWhip database removed (if it existed)");
+//	basic_query(initial_con, "CREATE DATABASE PartyWhip", "PartyWhip database created");
 	initial_con.connect(function(err) {
 		if (err) throw err;
 		console.log("initial con Connected!");
@@ -20,13 +23,8 @@ function initialise_database() {
 			if (err) throw err;
 			console.log("PartyWhip database created");
 		});
-		// new con
-		var con = mysql.createConnection({
-			host: "localhost",
-			user: "root",
-			password: "password",
-			database: "PartyWhip"
-		});
+	});
+
 	/*
 		con.connect(function(err) {
 			if (err) throw err;
@@ -37,16 +35,38 @@ function initialise_database() {
 			});
 		});
 	*/
-		basic_query(con, "CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");	
+	setTimeout( function() {
+		if (typeof callback == 'function') {
+			callback();
+		}}, 2000);
+}
+
+function create_tables() {
+	con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "password",
+		database: "PartyWhip"
 	});
+	/*	
+	con.connect(function(err) {
+		if (err) throw err;
+		console.log("con Connected!");
+		con.query("CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", function (err, result) {
+		if (err) throw err;
+			console.log("created Users table");
+		});
+	});
+	*/
+	basic_query("CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");	
 }
 
 
-function basic_query(connection, sql, message) {	
-	connection.connect(function(err) {
+function basic_query(sql, message) {	
+	con.connect(function(err) {
 		if (err) throw err;
 		console.log("Connected!");
-		connection.query(sql, function (err, result) {
+		con.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log(message);
 		});
--- model.js
+++ model.js
@@ -16,14 +16,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 var mysql = require('mysql');
+// ***** start of db stuff *****
+
+// var con;
 
 // setup PartyWhip database, !!! REMOVES CURRENT PARTYWHIP DATABASE !!!
-// initialise_database();
-
 /*
-initialise_database();
+initialise_database(create_tables);
 
-function initialise_database() {
+function initialise_database(callback) {
 	var initial_con = mysql.createConnection({
 		host: "localhost",
 		user: "root",
@@ -41,45 +42,38 @@ function initialise_database() {
 			if (err) throw err;
 			console.log("PartyWhip database created");
 		});
-		// new con
-		var con = mysql.createConnection({
-			host: "localhost",
-			user: "root",
-			password: "password",
-			database: "PartyWhip"
-		});
-	
-		con.connect(function(err) {
-			if (err) throw err;
-			console.log("con Connected!");
-			con.query("CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", function (err, result) {
-				if (err) throw err;
-				console.log("created Users table");
-			});
-		});
-	
-		basic_query(con, "CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");	
 	});
+	setTimeout( function() {
+		if (typeof callback == 'function') {
+			callback();
+		}}, 2000);
 }
 
-function basic_query(connection, sql, message) {	
-	connection.connect(function(err) {
+function create_tables() {
+	con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "password",
+		database: "PartyWhip"
+	});
+	basic_query("CREATE TABLE Users (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");	
+}
+
+
+function basic_query(sql, message) {	
+	con.connect(function(err) {
 		if (err) throw err;
 		console.log("Connected!");
-		connection.query(sql, function (err, result) {
+		con.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log(message);
 		});
 	});
 }
 
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "password",
-	database: "PartyWhip"
-});
 */
+// ***** end of db stuff *****
+
 app.get('/', function(req, res){
 	if (req.session.valid === undefined) req.session.valid = false;
 	if (req.session.error === undefined) req.session.error = false;
