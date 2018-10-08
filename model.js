0463480bdaf87b8186837ca2f8650f73214d195d
[JemZzz] <jemmaz1997@gmail.com>
2018-10-09 01:50:57 +1100

    search function

--- model.js
+++ model.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "password",
+	password: "niriksha",
 	database: "Seng"
 });
 
