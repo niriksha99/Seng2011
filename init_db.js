2d15d5877eb15d1117219858b85cd20845fd6377
BlaiseParadeza <z5160298@ad.unsw.edu.au>
2018-10-26 22:18:31 +1100

    link business time (if applicable) removed

--- init_db.js
+++ init_db.js
@@ -7,17 +7,17 @@ function initialise_database(callback) {
 	var initial_con = mysql.createConnection({
 		host: "localhost",
 		user: "root",
-		password: "niriksha"
+		password: "password"
 	});
 
 	initial_con.connect(function(err) {
 		if (err) throw err;
 		console.log("initial con Connected!");
-		initial_con.query("DROP DATABASE IF EXISTS abc", function (err, result) {
+		initial_con.query("DROP DATABASE IF EXISTS PartyWhip", function (err, result) {
 			if (err) throw err;
 			console.log("Old PartyWhip database removed (if it existed)");
 		});
-		initial_con.query("CREATE DATABASE abc", function (err, result) {
+		initial_con.query("CREATE DATABASE PartyWhip", function (err, result) {
 			if (err) throw err;
 			console.log("PartyWhip database created");
 		});
@@ -32,8 +32,8 @@ function create_tables() {
 	con = mysql.createConnection({
 		host: "localhost",
 		user: "root",
-		password: "niriksha",
-		database: "abc"
+		password: "password",
+		database: "PartyWhip"
 	});
 	basic_query("CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), phone_no VARCHAR(20), email VARCHAR(255))", "created Users table");
 
